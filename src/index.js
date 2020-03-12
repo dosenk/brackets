24a4c9161c92894bb38b78118abca5d612dea024
dosenk <eympel@gmail.com>
2020-03-12 16:47:59 +0300

    feat: completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var config = [],
+        new_str = str;
+
+    for (const elem of bracketsConfig) {
+        config.push(elem[0] + elem[1]);
+    }
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < config.length; j++) {
+            new_str = new_str.replace(config[j], '');
+        }
+        if (new_str.length == 0) {
+            return true;
+        }
+    }
+    return false;
 }
